833e621563780fd53dc5056d13ccd7d9cfb9bf44
tinuh <tinuvanapamula@gmail.com>
2020-05-03 15:15:25 -0400

    Animation Changes for collapse.

--- src/competitions-achievements.js
+++ src/competitions-achievements.js
@@ -21,25 +21,29 @@ function CompetitionsAchievements() {
         <hr color="#ffffff" />
 
             <Collapse in={year2020}>
-                <h3 className="competition">March - <a className = "competition-link" href = "https://secwww.jhuapl.edu/stem/mesa/winners/" target = "blank">3rd Place </a> JHU-APL MESA <a className = "competition-link" href = "https://secwww.jhuapl.edu/stem/mesa/school-based-program/competitions" target = "blank">NEDC Engineering Competition</a> (as <a className = "competition-link" href = "https://www.youtube.com/channel/UCO_5muSlEd5sDPZyKTuJbNA/" target = "blank">STEM Thunders</a>) </h3>
+                <div>
+                    <h3 className="competition">March - <a className = "competition-link" href = "https://secwww.jhuapl.edu/stem/mesa/winners/" target = "blank">3rd Place </a> JHU-APL MESA <a className = "competition-link" href = "https://secwww.jhuapl.edu/stem/mesa/school-based-program/competitions" target = "blank">NEDC Engineering Competition</a> (as <a className = "competition-link" href = "https://www.youtube.com/channel/UCO_5muSlEd5sDPZyKTuJbNA/" target = "blank">STEM Thunders</a>) </h3>
+                </div>
             </Collapse><br />
 
-        <h2 className="sub-heading"><Link to="#" className="toggle-collapse" aria-controls="example-collapse-text" aria-expanded={year2019} onClick={() => set2019(!year2019)}>2019</Link></h2>
+        <h2 className="sub-heading"><Link className="toggle-collapse" aria-controls="example-collapse-text" aria-expanded={year2019} onClick={() => set2019(!year2019)}>2019</Link></h2>
         <hr color="#ffffff"/>
 
-          <Collapse in={year2019}>
-              <h3 className="competition">
-                  December - Won the <a className="competition-link" href="https://www.congressionalappchallenge.us/" target="blank">Congressional App Challenge</a> for Maryland <a className="competition-link" href="https://www.congressionalappchallenge.us/19-MD03" target="blank">District MD-03</a><br />
-                  August - Got my 2nd Degree Black Belt in Tae-Kwon-Do (at <a className="competition-link" href="http://musamd.com" target="blank">Musa Martial Arts</a>)<br />
-                  March - 2nd Place for JHU-APL MESA <a className="competition-link" href="https://secwww.jhuapl.edu/stem/mesa/school-based-program/competitions" target="blank">Alice Middle School Coding Competition</a> (as <a className="competition-link" href="https://www.youtube.com/channel/UCO_5muSlEd5sDPZyKTuJbNA/" target="blank">STEM Thunders</a>)
-              </h3>
+          <Collapse in = {year2019} id = "2019">
+              <div>
+                  <h3 className="competition">December - Won the <a className="competition-link" href="https://www.congressionalappchallenge.us/" target="blank">Congressional App Challenge</a> for Maryland <a className="competition-link" href="https://www.congressionalappchallenge.us/19-MD03" target="blank">District MD-03</a></h3>
+                  <h3 className="competition">August - Got my 2nd Degree Black Belt in Tae-Kwon-Do (at <a className="competition-link" href="http://musamd.com" target="blank">Musa Martial Arts</a>)</h3>
+                  <h3 className="competition">March - 2nd Place for JHU-APL MESA <a className="competition-link" href="https://secwww.jhuapl.edu/stem/mesa/school-based-program/competitions" target="blank">Alice Middle School Coding Competition</a> (as <a className="competition-link" href="https://www.youtube.com/channel/UCO_5muSlEd5sDPZyKTuJbNA/" target="blank">STEM Thunders</a>)</h3>
+              </div>
           </Collapse><br/>
 
         <h2 className="sub-heading"><Link to="#" className="toggle-collapse" aria-controls="example-collapse-text" aria-expanded={year2016} onClick={() => set2016(!year2016)}>2016</Link></h2>
         <hr color="#ffffff"/>
 
           <Collapse in={year2016}>
-              <h3 className="competition">April - Got my 1st Degree Black Belt in Tae-Kwon-Do (at <a className="competition-link" href="http://musamd.com" target="blank">Musa Martial Arts</a>)</h3>
+              <div>
+                  <h3 className="competition">April - Got my 1st Degree Black Belt in Tae-Kwon-Do (at <a className="competition-link" href="http://musamd.com" target="blank">Musa Martial Arts</a>)</h3>
+              </div>
           </Collapse><br/>
 
       </div>
--- src/school.js
+++ src/school.js
@@ -23,11 +23,9 @@ function School() {
         <hr color="#ffffff" />
 
             <Collapse in={blair}>
-                <h4 className="school-text">Tinu Vanapamula is an Upcoming 9th grader at <a className="school-link"
-                                                                                            href="https://mbhs.edu/"
-                                                                                            target="blank">Montgomery
-                    Blair High School</a> for the 2020-2021 school year. He will be attending the Magnet Math, Science,
-                    and Computer Science Program there. </h4>
+                <div>
+                    <h4 className="school-text">Tinu Vanapamula is an Upcoming 9th grader at <a className="school-link" href="https://mbhs.edu/" target="blank">Montgomery Blair High School</a> for the 2020-2021 school year. He will be attending the Magnet Math, Science, and Computer Science Program there. </h4>
+                </div>
             </Collapse><br />
 
         <h2 className="sub-heading"><Link to = "#" className="toggle-collapse" aria-controls="example-collapse-text"
@@ -35,11 +33,9 @@ function School() {
         <hr color="#ffffff" />
 
             <Collapse in={tpms}>
-                <h4 className="school-text">Tinu Vanapamula is an Upcoming 9th grader at <a className="school-link"
-                                                                                            href="https://mbhs.edu/"
-                                                                                            target="blank">Montgomery
-                    Blair High School</a> for the 2020-2021 school year. He will be attending the Magnet Math, Science,
-                    and Computer Science Program there. </h4>
+                <div>
+                    <h4 className="school-text">Tinu Vanapamula is a current 8th grader at <a className="school-link" href="https://www.montgomeryschoolsmd.org/schools/takomaparkms/" target="blank">Takoma Park Middle School</a>. He is attending the Magnet Math, Science, and Computer Science Program there.</h4>
+                </div>
             </Collapse><br />
 
         <h2 className="sub-heading"><Link to = "#" className="toggle-collapse" aria-controls="example-collapse-text"
@@ -47,11 +43,9 @@ function School() {
         <hr color="#ffffff" />
 
             <Collapse in={drew}>
-                <h4 className="school-text">Tinu Vanapamula is an Upcoming 9th grader at <a className="school-link"
-                                                                                            href="https://mbhs.edu/"
-                                                                                            target="blank">Montgomery
-                    Blair High School</a> for the 2020-2021 school year. He will be attending the Magnet Math, Science,
-                    and Computer Science Program there. </h4>
+                <div>
+                    <h4 className="school-text">Tinu Vanapamula attended the Gifted and Talented Program at <a className="school-link" href="https://www2.montgomeryschoolsmd.org/schools/drewes/" target="blank">Dr. Charles R. Drew Elementary School</a> throughout his 4th and 5th grade years. From 2015 through 2017.</h4>
+                </div>
             </Collapse><br />
 
         <h2 className="sub-heading"><Link to = "#" className="toggle-collapse" aria-controls="example-collapse-text"
@@ -59,11 +53,9 @@ function School() {
         <hr color="#ffffff" />
 
             <Collapse in={bes}>
-                <h4 className="school-text">Tinu Vanapamula is an Upcoming 9th grader at <a className="school-link"
-                                                                                            href="https://mbhs.edu/"
-                                                                                            target="blank">Montgomery
-                    Blair High School</a> for the 2020-2021 school year. He will be attending the Magnet Math, Science,
-                    and Computer Science Program there. </h4>
+                <div>
+                    <h4 className="school-text">Tinu Vanapamula attended <a className="school-link" href="https://www2.montgomeryschoolsmd.org/schools/burtonsvillees/" target="blank">Burtonsville Elementary School</a> from Kindergarten all the way to 3rd grade.</h4>
+                </div>
             </Collapse>
       </div>
     </div>
