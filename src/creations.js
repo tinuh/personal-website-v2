f4d84781912768caa69eab1001b225a4a82812e3
tinu4 <tinuvanapamula@gmail.com>
2020-12-16 19:19:26 -0500

    remove default bootstrap

--- src/creations.js
+++ src/creations.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card } from "react-bootstrap";
 //import { CardDeck } from "react-bootstrap";
 import { Button } from "react-bootstrap";
